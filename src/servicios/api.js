cd78b4dff4ac5b82b46af7000735bf43af7f4842
gusmendez3249 <gustavoangelc2005@email.com>
2026-03-12 11:56:05 -0600

    Config Netlify

--- src/servicios/api.js
+++ src/servicios/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3001/api/v1"
+  baseURL: "https://backendpedidos-gusmendez3249s-projects.vercel.app/api/v1/"
 });
 
 // Interceptor para agregar token a todas las peticiones
